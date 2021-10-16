ea1ff99d7c0bceb26628076db55adcd5f8d945fb
Eduardo Reta Jr <dev.eddie.reta@gmail.com>
2021-10-16 12:24:10 -0400

    Campsite info component modified.

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,25 +1,21 @@
-import React, {Component} from "react";
 import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
 import CampsiteInfo from "./CampsiteInfoComponent";
 
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-           
-        };
-    };
-
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-sm-6">
-                    <Card onClick={() => this.props.onClick(campsite.id)} >
+function RenderDirectoryItem(campsite, onClick) {
+    return (
+        <Card onClick={() => this.props.onClick(campsite.id)} >
                     <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
                         </CardImgOverlay>
                     </Card>
+    )
+};
+function Directory(props) {
+        const directory = props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-sm-6">
+                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
                 </div>
             );
         });
@@ -33,6 +29,5 @@ class Directory extends Component {
             </div>
         );
     }
-};
 
 export default Directory;
