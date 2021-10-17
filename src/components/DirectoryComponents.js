8e803091d7f16358a7eee22a6af1b85797664d1b
Eduardo Reta Jr <https://github.com/Eddie-Reta>
2021-10-17 13:19:24 -0400

    React Router Parameters

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,34 +1,35 @@
 import React from "react";
-import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Link } from "react-router-dom";
+
 //import CampsiteInfo from "./CampsiteInfoComponent";
 
-function RenderDirectoryItem({campsite, onClick}) {
-    return (
-        <Card onClick={() => onClick(campsite.id)} >
-                    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-    )
-};
+function RenderDirectoryItem({ campsite }) {
+  return (
+    <Card>
+        <Link to={`/directory/${campsite.id}`}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Link>
+    </Card>
+  );
+}
 function Directory(props) {
-        const directory = props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-sm-6">
-                    <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
-                </div>
-            );
-        });
+  const directory = props.campsites.map((campsite) => {
+    return (
+      <div key={campsite.id} className="col-sm-6">
+        <RenderDirectoryItem campsite={campsite} />
+      </div>
+    );
+  });
 
-        return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
-               
-            </div>
-        );
-    }
+  return (
+    <div className="container">
+      <div className="row">{directory}</div>
+    </div>
+  );
+}
 
 export default Directory;
